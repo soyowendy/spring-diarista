df855da85c5a5bb5dc20bd4e2e47d67536775a32
Wenderson Santos <souowendy@gmail.com>
2023-08-23 13:52:22 -0300

    Initial commit

---
+++ src/main/java/com/soyowendy/ediarista/SistemaDeDiaristaApplication.java
@@ -0,0 +1,13 @@
+package com.soyowendy.ediarista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeDiaristaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeDiaristaApplication.class, args);
+	}
+
+}
