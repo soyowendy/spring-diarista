c7eff3b899e2c90dfebfda355dc488f94b333e97
Wenderson Santos <souowendy@gmail.com>
2023-08-24 13:19:26 -0300

    Create Usuario register route

--- src/main/java/com/soyowendy/ediarista/web/controllers/UsuarioController.java
+++ src/main/java/com/soyowendy/ediarista/web/controllers/UsuarioController.java
@@ -1,11 +1,18 @@
 package com.soyowendy.ediarista.web.controllers;
 
+import com.soyowendy.ediarista.web.dtos.FlashMessageDTO;
+import com.soyowendy.ediarista.web.dtos.UsuarioCadastroFormDTO;
 import com.soyowendy.ediarista.web.services.WebUsuarioService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/admin/usuarios")
@@ -18,5 +25,26 @@ public class UsuarioController {
 		ModelAndView modelAndView = new ModelAndView("admin/usuario/lista");
 		modelAndView.addObject("usuarios", usuarioService.buscarTodos());
 		return modelAndView;
+	}
+
+	@GetMapping("/cadastrar")
+	public ModelAndView cadastrar() {
+		ModelAndView modelAndView = new ModelAndView("admin/usuario/cadastro-form");
+		modelAndView.addObject("cadastroForm", new UsuarioCadastroFormDTO());
+		return modelAndView;
+	}
+
+	@PostMapping("/cadastrar")
+	public String cadastrar(@Valid @ModelAttribute("cadastroForm") UsuarioCadastroFormDTO form,
+	                        BindingResult result,
+	                        RedirectAttributes attrs) {
+		if (result.hasErrors()) {
+			return "admin/usuario/cadastro-form";
+		}
+
+		usuarioService.cadastrar(form);
+		attrs.addFlashAttribute("alert", new FlashMessageDTO("alert-success", "Usuário cadastrado com sucesso!"));
+
+		return "redirect:/admin/usuarios";
 	}
 }
--- src/main/java/com/soyowendy/ediarista/web/services/WebUsuarioService.java
+++ src/main/java/com/soyowendy/ediarista/web/services/WebUsuarioService.java
@@ -1,7 +1,10 @@
 package com.soyowendy.ediarista.web.services;
 
+import com.soyowendy.ediarista.core.enums.TipoUsuario;
 import com.soyowendy.ediarista.core.models.Usuario;
 import com.soyowendy.ediarista.core.repositories.UsuarioRepository;
+import com.soyowendy.ediarista.web.dtos.UsuarioCadastroFormDTO;
+import com.soyowendy.ediarista.web.mappers.WebUsuarioMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +15,18 @@ public class WebUsuarioService {
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 
+	@Autowired
+	private WebUsuarioMapper mapper;
+
 	public List<Usuario> buscarTodos() {
 		return usuarioRepository.findAll();
+	}
+
+	public Usuario cadastrar(UsuarioCadastroFormDTO form) {
+		Usuario model = mapper.toModel(form);
+
+		model.setTipoUsuario(TipoUsuario.ADMIN);
+
+		return usuarioRepository.save(model);
 	}
 }
