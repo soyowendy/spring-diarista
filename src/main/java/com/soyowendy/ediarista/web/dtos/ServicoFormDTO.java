4f06d01b9b2b5736cbc7647428d96aa748166db9
Wenderson Santos <souowendy@gmail.com>
2023-08-23 21:42:54 -0300

    Apply Mask to number

--- src/main/java/com/soyowendy/ediarista/web/dtos/ServicoFormDTO.java
+++ src/main/java/com/soyowendy/ediarista/web/dtos/ServicoFormDTO.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.NumberFormat;
 
 import java.math.BigDecimal;
 
@@ -17,6 +18,7 @@ public class ServicoFormDTO {
 	private String nome;
 	@NotNull
 	@PositiveOrZero
+	@NumberFormat(style = NumberFormat.Style.CURRENCY, pattern = "#,##0.00")
 	private BigDecimal valorMinimo;
 	@NotNull
 	@PositiveOrZero
@@ -24,42 +26,49 @@ public class ServicoFormDTO {
 	@NotNull
 	@PositiveOrZero
 	@Max(100)
+	@NumberFormat(style = NumberFormat.Style.CURRENCY, pattern = "#,##0.00")
 	private BigDecimal porcentagemComissao;
 	@NotNull
 	@PositiveOrZero
 	private Integer horasQuarto;
 	@NotNull
 	@PositiveOrZero
+	@NumberFormat(style = NumberFormat.Style.CURRENCY, pattern = "#,##0.00")
 	private BigDecimal valorQuarto;
 	@NotNull
 	@PositiveOrZero
 	private Integer horasSala;
 	@NotNull
 	@PositiveOrZero
+	@NumberFormat(style = NumberFormat.Style.CURRENCY, pattern = "#,##0.00")
 	private BigDecimal valorSala;
 	@NotNull
 	@PositiveOrZero
 	private Integer horasBanheiro;
 	@NotNull
 	@PositiveOrZero
+	@NumberFormat(style = NumberFormat.Style.CURRENCY, pattern = "#,##0.00")
 	private BigDecimal valorBanheiro;
 	@NotNull
 	@PositiveOrZero
 	private Integer horasCozinha;
 	@NotNull
 	@PositiveOrZero
+	@NumberFormat(style = NumberFormat.Style.CURRENCY, pattern = "#,##0.00")
 	private BigDecimal valorCozinha;
 	@NotNull
 	@PositiveOrZero
 	private Integer horasQuintal;
 	@NotNull
 	@PositiveOrZero
+	@NumberFormat(style = NumberFormat.Style.CURRENCY, pattern = "#,##0.00")
 	private BigDecimal valorQuintal;
 	@NotNull
 	@PositiveOrZero
 	private Integer horasOutros;
 	@NotNull
 	@PositiveOrZero
+	@NumberFormat(style = NumberFormat.Style.CURRENCY, pattern = "#,##0.00")
 	private BigDecimal valorOutros;
 	@NotNull
 	private Icone icone;
