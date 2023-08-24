49c2885952162b4ff402cacf6e65803c69036280
Wenderson Santos <souowendy@gmail.com>
2023-08-24 17:57:36 -0300

    Update Security configuration

--- src/main/java/com/soyowendy/ediarista/config/SecurityConfig.java
+++ src/main/java/com/soyowendy/ediarista/config/SecurityConfig.java
@@ -1,18 +1,30 @@
 package com.soyowendy.ediarista.config;
 
+import com.soyowendy.ediarista.core.enums.TipoUsuario;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfig  {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.authorizeHttpRequests((auth) -> {
-			auth.anyRequest().permitAll();
-		});
+		http
+			.authorizeHttpRequests((auth) -> {
+				auth.requestMatchers("/admin/**").hasAuthority(TipoUsuario.ADMIN.toString());
+				auth.anyRequest().authenticated();
+			})
+			.formLogin(Customizer.withDefaults())
+			.logout((logout) -> logout.logoutRequestMatcher(new AntPathRequestMatcher("/admin/logout", "GET")));
 		return http.build();
+	}
+
+	@Bean
+	public WebSecurityCustomizer webSecurityCustomizer() {
+		return (web) -> web.ignoring().requestMatchers("/webjar/**");
 	}
 }
