f11fb29182aeedbb3e06432cc70586b6864d4a91
Wenderson Santos <souowendy@gmail.com>
2023-08-24 22:53:56 -0300

    Configure Token Validation Remember

--- src/main/java/com/soyowendy/ediarista/config/SecurityConfig.java
+++ src/main/java/com/soyowendy/ediarista/config/SecurityConfig.java
@@ -1,9 +1,9 @@
 package com.soyowendy.ediarista.config;
 
 import com.soyowendy.ediarista.core.enums.TipoUsuario;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,6 +11,11 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfig  {
+	@Value("${com.soyowendy.ediarista.rememberMe.key}")
+	private String rememberMeKey;
+	@Value("${com.soyowendy.ediarista.validitySeconds}")
+	private int rememberMeValiditySeconds;
+
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
@@ -22,7 +27,9 @@ public class SecurityConfig  {
 					form.loginPage("/admin/login").permitAll().usernameParameter("email").passwordParameter("senha")
 							.defaultSuccessUrl("/admin/servicos"))
 			.logout((logout) ->
-					logout.logoutRequestMatcher(new AntPathRequestMatcher("/admin/logout", "GET")));
+					logout.logoutRequestMatcher(new AntPathRequestMatcher("/admin/logout", "GET")))
+			.rememberMe((remember) ->
+					remember.tokenValiditySeconds(rememberMeValiditySeconds).key(rememberMeKey).rememberMeParameter("lembrar"));
 		return http.build();
 	}
 
